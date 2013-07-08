14fb0e658e645315c1bc5fc24f74b7b4c3e24e6e
Javier Torcelly <javiersson@gmail.com>
2013-07-09 00:47:59 +0200

    Modified to read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var fs = require('fs');
+
+var buffer = new Buffer(64);
+buffer = fs.readFileSync('./index.html');
 
 var app = express.createServer(express.logger());
+  app.get('/', function(request, response) {
+  response.send(buffer.toString('utf-8'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+//  console.log("buffer.length: " + buffer.length);
+//  console.log(buffer.toString('utf-8'));
 });
