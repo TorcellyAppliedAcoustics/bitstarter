b65b20280767c0162c68217c796922fcbd7460cb
Javier Torcelly <javiersson@gmail.com>
2013-07-19 13:05:40 +0200

    Updated index.html according to HW4.Part1

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ var app = express.createServer(express.logger());
 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 //  console.log("buffer.length: " + buffer.length);
